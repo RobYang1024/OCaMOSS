060697eb157f57d0dceb78f2cf382e4cc91a1d84
aniroodh-ravikumar <ar765@cornell.edu>
2018-03-29 21:36:47 -0400

    More preprocessing changes

--- preprocessing.mli
+++ preprocessing.mli
@@ -1,9 +1,13 @@
 
-
+(* A [Comparable] is a value that can be compared. *)
 module type Comparable = sig
 
+  (* Type t gives the type of the value that is stored in the comparable. *)
   type t
 
+(* [compare t1 t2] returns a positive number if t1 is greater than t2,
+ * a negative number if it is less than t2 and 0 if they are equal.
+ *)
   val compare: t -> t -> int
 
 end
@@ -20,25 +24,39 @@ module type Dictionary = sig
 
   type value
 
+(* Type t represents how the dictionary itself is implemented and stored as.
+ *)
   type t
 
+  (* [empty] represents an empty dictionary. *)
   val empty : t
 
+  (* [member k dict] returns true if k is a binding in the dictionary d
+   * and false if not.
+   *)
   val member : key -> t -> bool
 
+  (* [find k dict] returns None if k is not a binding in dict, and Some v
+   * if v is the value bound to k in dict.
+   *)
   val find : key -> t -> value option
 
+  (* [insert k v dict] adds a new binding of k with value v to dict if k is not
+   * in dict already, and binds k to a new value v if it is already
+   * present in the dictionary, and returns the new dictionary.
+   *)
   val insert : key -> int -> t -> t
 
+  (* [remove k dict] returns dict with binding k removed from it if it is
+   * a binding in dict, and dict as it is otherwise.
+   *)
   val remove : key -> t -> t
 
+  (* [to_list dict] returns an association list where each element of the list
+   * is a tuple of a key and it's corresponding value in dict. *)
   val to_list : t -> (key * value) list
 
 end
-
-type ('a,'b) avl =
-  | Leaf
-  | Node of ('a * 'b) * ('a,'b) avl * ('a,'b) avl * int
 
 (* An implementation of dictionary using avl trees. *)
 module TreeDictionary: Dictionary
