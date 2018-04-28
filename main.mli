fd0f45a59d4584504c236aaaa2a8206c64ce089b
yangdanny97 <dzy4@cornell.edu>
2018-04-27 21:46:25 -0400

    started work on the REPL

--- main.mli
+++ main.mli
@@ -1,3 +1,13 @@
 open Preprocessing
 open Comparison
 open Interface
+
+type state = {display : string; directory : string; results : unit}
+
+type cmd = RUN | HELP | SETDIR | RESULTS | COMPARE | ERROR
+
+type input = cmd * string option * string option
+
+val parse: string -> input
+
+val repl : state -> unit
